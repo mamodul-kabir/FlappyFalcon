7eb8f15b79e40019a7263243714c7ce71697b495
evm6279 <evm6279@nyu.edu>
2025-12-04 12:33:55 -0500

    updated the login and signup pages
    just added some error handling and styling

--- code/signup_page.php
+++ code/signup_page.php
@@ -1,18 +1,25 @@
 <?php
-    $stylesheet_url = "css/signinup.css"; // Define the specific stylesheet
-    include 'header.php';           // Include the header
-
-    if(isset($_POST["signup"])){
-        header('Location: login.php'); 
-    }
+    $stylesheet_url = "css/signinup.css";
+    include 'header.php';
 ?>
+
+<?php if (isset($_GET['error'])): ?>
+    <p style="color: red;">
+        <?php
+            if ($_GET['error'] == 'empty') echo "Please enter username and password.";
+            else if ($_GET['error'] == 'exists') echo "Username already taken.";
+            else if ($_GET['error'] == 'failed') echo "Signup failed. Please try again.";
+        ?>
+    </p>
+<?php endif; ?>
 
 <form action="signup.php" method="post">
     <label>Username: <input type="text" name="uname" id="uname"></label><br>
-    <label>Password <input type="text" name="pw" id="pw"></label><br>
+    <label>Password: <input type="password" name="pw" id="pw"></label><br>
     <input type="submit" name="signup" value="signup">
 </form>
-    <a href="login.php"><button>login</button></a>
+<a href="login_page.php"><button>login</button></a>
+
 <?php
     include "footer.php"; 
 ?>
